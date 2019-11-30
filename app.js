9eb2e4309506883cc458e3774c7f573bacaeb0a5
atjhoendZ <achunarmando@gmail.com>
2019-12-01 04:14:30 +0700

    from / to /api

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ db.once('open', function(callback){
 
 
 var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -36,8 +35,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
